730ece3d2d0ab6e0f3628f9fbcf7ad0dfc766e0b
Valentin Guschin <112811051+valogus@users.noreply.github.com>
2023-03-01 11:57:57 +0400

    main task

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Tabs from './components/Tabs.vue'
+import Tab from './components/Tab.vue'
+</script>
+
+<template>
+    <Tabs>
+        <Tab title="Vue">Content of Vue Tab</Tab>
+        <Tab title="HTML">Content of HTML Tab</Tab>
+        <Tab title="SCSS">Content of SCSS Tab</Tab>
+    </Tabs>
+</template>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { inject } from 'vue'
+
+const props = defineProps({
+    title: String
+})
+
+const selectedTitle = inject('selectedTitle')
+</script>
+
+<template>
+    <div class="tab__content" v-show="selectedTitle === title">
+        <slot />
+    </div>
+</template>
+
+<style>
+.tab__content {
+    background-color: #bfbfbf;
+    min-height: 300px;
+    display: grid;
+    place-items: center;
+    border-radius: 0 0 5px 5px;
+    padding: 10px;
+}
+</style>
